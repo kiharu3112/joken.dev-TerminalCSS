d90250e04b1528cfbb04b93cd599203156a8af98
kiharu3112 <kiharu3112@gmail.com>
2023-11-27 01:00:57 +0900

    achievements

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,84 @@
+<script>
+  import 'terminal.css'
+  import { page } from '$app/stores'
+</script>
+<header>
+  <a href="/" class="link">NITMC JOKEN</a>
+</header>
+
+<div id="body">
+  <div id="pwd">
+    <p class="code">pwd</p>
+    <div class="pwd-result">
+      <a href="/">/</a>
+      {#if $page.url.pathname === "/achievements"}
+      <a href="/achievements">achievements</a>
+      {/if}
+      {#if $page.url.pathname === "/members"}
+      <a href="/members">Members</a>
+      {/if}
+      {#if $page.url.pathname === "/activity"}
+      <a href="/activity">Activities</a>
+      {/if}
+      {#if $page.url.pathname === "/works"}
+      <a href="/works">Contact</a>
+      {/if}
+      {#if $page.url.pathname === "/join"}
+      <a href="/join">Join</a>
+      {/if}
+    </div>
+  </div>
+  <div id="ls">
+    <p class="code">ls /</p>
+    <div class="ls-result">
+      <a href="/achievements">achievements</a>
+      <a href="/members">members</a>
+      <a href="/activity">activity</a>
+      <a href="/works">works</a>
+      <a href="/join">join</a>
+      <a href="https://joken.dev">joken.dev</a>
+    </div>
+  </div>
+  <slot />
+</div>
+<style>
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    max-width: 800px;
+    margin: 0 auto;
+    padding-top: 0.3rem;
+  }
+  a {
+    margin-right: 0.5rem;
+  }
+  .link {
+    font-size: 1rem;
+    font-weight: 600;
+    margin: 0;
+    padding: 0;
+  }
+  .link::before {
+    content: '> ';
+  }
+  .code {
+    color: #287100;
+    margin: 0;
+  }
+  .code::before {
+    content: '$ ';
+    color: #000000
+  }
+  .pwd-result {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    margin-bottom: 1rem;
+  }
+  #body {
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 1rem 0;
+  }
+</style>
---
+++ src/routes/achievements/+page.svelte
@@ -0,0 +1,45 @@
+<ul>
+  <li>中高生国際Rubyプログラミングコンテスト 2022 in Mitaka
+    <ul>
+      <li>優秀賞</li>
+      <li>スポンサー賞</li>
+    </ul>
+  </li>
+  <li>JOI 2021/2022
+    <ul>
+      <li>本選出場</li>
+    </ul>
+  </li>
+  <li>中国地区高専コンピュータフェスティバル2022 matsue
+    <ul>
+      <li>メディアコンテンツ部門3位</li>
+      <li>アプリケーション部門3位</li>
+    </ul>
+  </li>
+  <li>
+    中国地区高専コンピュータフェスティバル2022 oshima
+    <ul><li>部長賞</li></ul>
+  </li>
+  <li>高専プログラミングコンテスト2023
+    <ul>
+      <li>課題部門 Pixiv賞</li>
+      <li>自由部門 特別賞 トヨタシステムズ賞</li>
+    </ul>
+  </li>
+</ul>
+<div class="loading"></div>
+<style>
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+  li {
+    margin: 0.5rem 0;
+  }
+  li ul {
+    margin: 0.5rem 0 0 1rem;
+  }
+  li ul li {
+    margin: 0.5rem 0;
+  }
+</style>
