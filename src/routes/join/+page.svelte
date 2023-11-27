16faede5bb1750cab2cf37beaf432db51484dd50
kiharu3112 <kiharu3112@gmail.com>
2023-11-27 18:21:10 +0900

    join

---
+++ src/routes/join/+page.svelte
@@ -0,0 +1,57 @@
+<header>入部案内</header>
+<div class="terminal-card">
+  <header>自分のPCを持ってくる必要はありますか</header>
+  <p>
+    見学と入学すぐのときはNo、それ以外はYesです。<br>
+    持っていない場合をのぞいて、活動にはPCを持ってきてもらいます。
+    難しい場合は相談した上で部が保有しているPCを使うこともできます。
+  </p>
+</div>
+<div class="terminal-card">
+  <header>情報工学科(J科)でなくても入部できますか？</header>
+  <p>
+    できます。現在部員はD,E,J科の学生で構成されています。
+  </p>
+</div>
+<div class="terminal-card">
+  <header>プログラミング未経験でも入部できますか？</header>
+  <p>
+    できます。<br>
+    4~8月にかけて毎年プログラミング講習会を実施しています。<br>
+    それ以外にも部員同士で教え合うこともしています。
+  </p>
+</div>
+<div class="terminal-card">
+  <header>部活の掛け持ちは可能ですか?</header>
+  <p>
+    可能です。
+  </p>
+</div>
+<div class="terminal-card">
+  <header>自分のPCを持ってくる必要はありますか</header>
+  <p>
+    いいえ。部費を部員から徴収することはしていません。<br>
+    大会参加費などは随時発生する可能性があります。
+  </p>
+</div>
+<div class="terminal-card">
+  <header>2年生以上でも入部できますか?</header>
+  <p>
+    はい。1~5年生であれば誰でも入部できます。
+  </p>
+</div>
+<div class="terminal-card">
+  <header>入部の手続きの仕方を教えてください</header>
+  <p>
+    活動日に活動場所に来てください。<br>
+    部で利用しているDiscordサーバーに招待します。
+  </p>
+</div>
+<style>
+  header, p {
+    font-family: 'UDEVGothicNF';
+  }
+  .terminal-card {
+    margin: 1rem 0;
+  }
+</style>
