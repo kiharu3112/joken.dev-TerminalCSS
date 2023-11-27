72ef346a5a7cbf68e52f3fa9eddcafbc9052df14
kiharu3112 <kiharu3112@gmail.com>
2023-11-27 18:48:59 +0900

    works

---
+++ src/routes/works/+page.svelte
@@ -0,0 +1,76 @@
+<header>作品一覧</header>
+<div class="terminal-card">
+  <header>JKOJS</header>
+  <img src="https://raw.githubusercontent.com/poporonnet/kojs6-backend/v1/docs/img/jk-ojs.jpg" alt="jkojs"/>
+  <p>
+    オンラインジャッジと呼ばれる、競技プログラミングの大会を開催するのに必要なシステムの実装です。TypeScriptとGoで書かれています。
+    <a href="https://github.com/C4NoN424">
+      <button class="btn btn-default">
+        GitHub
+      </button>
+    </a>
+  </p>
+</div>
+<div class="terminal-card">
+  <header>pnket</header>
+  <p>
+    ただ別のチームのプロジェクトにするつもりだったプロジェクトの最初の部分を作っただけです。パッケージマネージャに特化した仕様になっています。
+    <a href="https://github.com/Ablaze-MIRAI/pnket">
+      <button class="btn btn-default">
+        GitHub
+      </button>
+    </a>
+  </p>
+</div>
+<div class="terminal-card">
+  <header>#B43A6EEA</header>
+  <img src="https://i.ytimg.com/vi/fvJEkvtvan4/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDgAASKEIzS6ilxK76pLVHiV5cIoQ" alt="#B43A6EEA"/>
+  <p>
+    量産型Future Bassです。LFOをうまく使ってSuper Sawを動かしたりしました。
+    <a href="hhttps://www.youtube.com/watch?v=fvJEkvtvan4">
+      <button class="btn btn-error">
+        Youtube
+      </button>
+    </a>
+  </p>
+</div>
+<div class="terminal-card">
+  <header>#B43A6EEA</header>
+  <img src="https://cdn.discordapp.com/attachments/964378851163656232/1090244187586314301/49.png" alt="box run"/>
+  <p>
+    カードゲームを作っていたはずが気づいたらアクションゲームになっていた。そのうちきちんと完成させます。使用言語はRuby
+  </p>
+</div>
+<div class="terminal-card">
+  <header>マ。feat.可不</header>
+  <img src="https://media.discordapp.net/attachments/964378851163656232/1091663145325580358/bandicam_2023-01-11_23-03-43-242.jpg" alt="マ。feat.可不"/>
+  <p>
+    CeVIO AIの可不に歌ってもらった楽曲です。
+    <a href="https://www.youtube.com/watch?v=fvJEkvtvan4">
+      <button class="btn btn-error">
+        Youtube
+      </button>
+    </a>
+  </p>
+</div>
+<div class="terminal-card">
+  <header>WashBoard</header>
+  <img src="https://www.notion.so/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2F807403ae-affd-476f-95db-85cc30024b58%2F0c5c2cb3-2766-417c-9345-01d7fa496202%2Fwashboard-app-title.png?table=block&id=eaf21e1b-a96a-4581-8a58-45808a48ddc3&spaceId=807403ae-affd-476f-95db-85cc30024b58&width=1610&userId=435b30ee-c494-48ae-a622-1918a5e8974b&cache=v2" alt="WashBoard"/>
+  <p>
+    学生寮の洗濯管理をスマートに。プロコン34自由部門。
+    <a href="https://github.com/mct-joken/WashBoard/tree/main">
+      <button class="btn btn-default">
+        GitHub
+      </button>
+    </a>
+  </p>
+</div>
+
+<style>
+  header, p {
+    font-family: 'UDEVGothicNF';
+  }
+  .terminal-card {
+    margin: 1rem 0;
+  }
+</style>
