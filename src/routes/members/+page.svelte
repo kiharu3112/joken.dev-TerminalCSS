e9be641cb0935530905e00e8d199d65e1aa832ae
kiharu3112 <kiharu3112@gmail.com>
2023-11-27 17:23:04 +0900

    members

---
+++ src/routes/members/+page.svelte
@@ -0,0 +1,121 @@
+<div id="members">
+  <div class="terminal-card">
+    <header>
+      laminne
+    </header>
+    <div>
+      <p>Tatsuto YAMAMOTO</p>
+      <p>元部長</p>
+      <div>
+        <a href="https://github.com/laminne">
+          <button class="btn btn-default">
+            GitHub
+          </button>
+        </a>
+        <button class="btn btn-primary">
+          Twitter
+        </button>
+      </div>
+    </div>
+  </div>
+  <div class="terminal-card">
+    <header>
+      kijiharu3112
+    </header>
+    <div>
+      <p>kiharu3112</p>
+      <p>家畜</p>
+      <div>
+        <a href="https://github.com/kiharu3112">
+          <button class="btn btn-default">
+            GitHub
+          </button>
+        </a>
+      </div>
+    </div>
+  </div>
+  <div class="terminal-card">
+    <header>
+      Piano Nakanoin
+    </header>
+    <div>
+      <p>Piano Nakanoin</p>
+      <p>RUST LAB</p>
+      <div>
+        <a href="https://github.com/NSK-1010">
+          <button class="btn btn-default">
+            GitHub
+          </button>
+        </a>
+        <a href="https://github.com/NSK-1010">
+          <button class="btn btn-primary">
+            Twitter
+          </button>
+        </a>
+        <a href="https://www.youtube.com/@nsk-1010">
+          <button class="btn btn-error">
+            YouTube
+            </button>
+        </a>
+      </div>
+    </div>
+  </div>
+  <div class="terminal-card">
+    <header>
+      hamaann
+    </header>
+    <div>
+      <p>hamaann</p>
+      <p>創作に生きる亀</p>
+      <div>
+        <a href="https://github.com/hamaann">
+          <button class="btn btn-default">
+            GitHub
+          </button>
+        </a>
+        <a href="https://twitter.com/LljKDJzur8Be327">
+          <button class="btn btn-primary">
+            Twitter
+          </button>
+        </a>
+      </div>
+    </div>
+  </div>
+  <div class="terminal-card">
+    <header>
+      Canon
+    </header>
+    <div>
+      <p>Canon424</p>
+      <p>蝨ｨ邀榊ｹｴ謨ｰ繝医ャ繝�</p>
+      <div>
+        <a href="https://github.com/C4NoN424">
+          <button class="btn btn-default">
+            GitHub
+          </button>
+        </a>
+        <a href="https://twitter.com/@c4n4242">
+        <button class="btn btn-primary">
+          Twitter
+        </button>
+      </a>
+      </div>
+    </div>
+  </div>  
+</div>
+
+<style>
+  p {
+    font-family: 'UDEVGothicNF';
+  }
+  .terminal-card {
+    width: 20rem;
+    height: 14rem;
+    margin: 1rem auto;
+  }
+  #members {
+    flex-wrap: wrap;
+    display: inline-flex;
+    justify-content: space-between;
+  }
+</style>
